a67882add76410ff06083b0e69549b6f5eb24ec0
ianyiern <31889778+poloke@users.noreply.github.com>
2022-07-28 14:43:35 +0800

    Add files via upload

---
+++ src/main/java/sg/edu/iss/kuruma/controller/SearchController.java
@@ -0,0 +1,44 @@
+package sg.edu.iss.kuruma.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import sg.edu.iss.kuruma.model.Car;
+import sg.edu.iss.kuruma.service.CarService;
+
+@Controller
+public class SearchController {
+	
+	@Autowired
+	CarService cservice;
+	
+	@RequestMapping("/home")
+	public String home(Model model){
+		Car car1 = new Car();
+		model.addAttribute("car", car1);
+		return "home";
+	}
+	
+	
+    @PostMapping("/search")
+	public String searchBrand(@ModelAttribute("car") Car car, Model model) {
+		String brand = car.getBrand();
+		List<Car> list = cservice.findByBrand(brand);
+		model.addAttribute("brandlist",list);
+		System.out.println(car.getBrand());
+		return "brandlist";
+	}
+
+    /*@RequestMapping(value = "/list")
+	public String list(Model model) {
+		model.addAttribute("cars", cservice.listCar());
+		return "cars";
+	}*/
+}
