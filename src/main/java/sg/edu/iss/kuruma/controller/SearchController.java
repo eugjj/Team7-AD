b76f9b5e80d80b4d87fa7ff1c9135c6d2f73c22f
Eugene Ong <eugeneongjj@gmail.com>
2022-08-01 17:27:13 +0800

    merged w nwe and thiri, cardetails and paging done and working

--- src/main/java/sg/edu/iss/kuruma/controller/SearchController.java
+++ src/main/java/sg/edu/iss/kuruma/controller/SearchController.java
@@ -1,5 +1,6 @@
 package sg.edu.iss.kuruma.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import sg.edu.iss.kuruma.model.Car;
 import sg.edu.iss.kuruma.service.CarService;
@@ -20,6 +22,9 @@ public class SearchController {
 	
 	@Autowired
 	CarService cservice;
+	
+	private List<Car>listByPage;
+	private final int ITEMS_PER_PAGE = 20;
 	
 	 @RequestMapping("/home") public String home(Model model, @Param("entry") String entry) { 
 		 model.addAttribute("entry", entry); 
@@ -35,12 +40,54 @@ public class SearchController {
 		}
 	
     @PostMapping("/search")
-	public String searchCar(@Param("entry") String entry, Model model) {
-    	List<Car> list = cservice.findSearchByEntry(entry);
-    	model.addAttribute("searchlist",list);
+	public String searchCar(@RequestParam("entry") String entry, Model model) {
+    	listByPage = new ArrayList<Car>();
+    	listByPage = cservice.findSearchByEntry(entry);
+    	List<Car> result = new ArrayList<Car>();
+    	if (listByPage.size() > 0) {
+    		for(int i=0; i<ITEMS_PER_PAGE; i++) {
+    			result.add(listByPage.get(i));}}
+    	
+    	model.addAttribute("searchlist",result);
     	model.addAttribute("entry", entry);
+    	model.addAttribute("currentPage",1);
 		return "searchlist";
 	}
+    
+    @RequestMapping("/search/forward/{currentPage}")
+    public String searchCarsNext(@PathVariable(value = "currentPage") int page, Model model) {
+    	List<Car> result = new ArrayList<Car>();
+    	if (listByPage.size() / ITEMS_PER_PAGE < page) {
+    		page--;
+    	
+    	}
+    	else if (ITEMS_PER_PAGE * page == listByPage.size()) {
+    		page--;
+    	}
+    	if (listByPage.size() > 0) {
+    		if ((listByPage.size() / ITEMS_PER_PAGE) + 1 >= page) {
+    			int start = page * ITEMS_PER_PAGE;
+    			int max = ++page * ITEMS_PER_PAGE;
+    			if (max > listByPage.size()) {
+    				max = listByPage.size();
+    			}
+    			for(int i=start; i<max; i++) {
+    				result.add(listByPage.get(i));
+    			}
+    		}
+    	}
+    	model.addAttribute("currentPage", page);
+    	model.addAttribute("searchlist", result);
+    	return "searchlist"; 
+    	}
+    
+    @RequestMapping("/cardetail/{id}")
+    public String cardetail(@PathVariable("id") Integer id, Model model) {      
+        Car cardetails = cservice.findById(id);
+        model.addAttribute("carD",cardetails);
+        return "cardetail";
+    }
+    
 
     @RequestMapping("/contactus")
     public String home() {
